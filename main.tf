7733a715950c4b387a43a55da0cd83ef923b8b9a
Ali Ahsan <samnaniaa@gmail.com>
2023-06-20 23:17:39 +0500

    modified

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -97,6 +98,15 @@ output "aws_ami" {
   value = data.aws_ami.latest_ubuntu_image.id
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "server-key" {
+  key_name = "server-key"
+  public_key = "${file(var.public_key_location)}"
+}
+
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest_ubuntu_image.id
   instance_type = var.instance_type
@@ -104,5 +114,10 @@ resource "aws_instance" "myapp-server" {
   vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone = var.avail_zone
   associate_public_ip_address = true
+  key_name = aws_key_pair.server-key.key_name
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 }
 
