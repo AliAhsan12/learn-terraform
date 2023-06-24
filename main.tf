7c87cd02ee10039fedc636453a7837cac4557c09
Ali Ahsan <samnaniaa@gmail.com>
2023-06-24 17:09:48 +0500

    created modules

--- main.tf
+++ main.tf
@@ -1,16 +1,6 @@
-
 provider "aws" {}
 
-
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-variable "private_key_location" {}
-
+# vpc
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -18,131 +8,26 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+# subnet module
+module "subnet_module" {
+  source            = "./modules/subnet"
   vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone        = var.avail_zone
+  env_prefix        = var.env_prefix
 }
 
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-route-table"
-  }
+# webserver module
+module "webserver_module" {
+  source               = "./modules/webserver"
+  vpc_id               = aws_vpc.myapp-vpc.id
+  image_name           = var.image_name
+  image_owner          = var.image_owner
+  public_key_location  = var.public_key_location
+  private_key_location = var.private_key_location
+  instance_type        = var.instance_type
+  subnet_id            = module.subnet_module.subnet_id
+  avail_zone           = var.avail_zone
+  env_prefix           = var.env_prefix
+  my_ip                = var.my_ip
 }
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_route_table_association" "a-rt-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
-}
-
-resource "aws_security_group" "myapp-sg" {
-  name   = "myapp-sg"
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-sg"
-  }
-}
-
-data "aws_ami" "latest_ubuntu_image" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/*ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical account ID
-}
-output "aws_ami" {
-  value = data.aws_ami.latest_ubuntu_image.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
-
-resource "aws_key_pair" "server-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami                         = data.aws_ami.latest_ubuntu_image.id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
-  availability_zone           = var.avail_zone
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.server-key.key_name
-
-  user_data = <<-EOF
-  #!/bin/sh 
-  sudo apt update -y 
-  sudo apt install docker.io -y
-  sudo apt install apache2 -y
-  sudo cp /tmp/index.html /var/www/html/index.html
-  sudo systemctl start docker
-  sudo usermod -a -G docker ubuntu
-  docker run -p 8080:80 nginx
-  EOF
-
-
- provisioner "file" {
-   source = "index.html"
-   destination = "/tmp/index.html"
-   connection {
-     type        = "ssh"
-     user = "ubuntu"
-     private_key = file("${var.private_key_location}")
-     host = "${self.public_ip}"
-   }
- }
-
-
-  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
-}
-
