44dc0e4752fbea92d4e378428c67c23af2c918e5
Ali Ahsan <samnaniaa@gmail.com>
2023-06-24 13:11:42 +0500

    modified

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
+variable "private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -18,8 +19,8 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
     Name = "${var.env_prefix}-subnet-1"
@@ -45,32 +46,32 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 resource "aws_route_table_association" "a-rt-subnet" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id      = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
 }
 
 resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+  name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
 
-  ingress  {
+  ingress {
     from_port   = 22
-    to_port = 22
-    protocol = "tcp"
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
 
-   ingress  {
+  ingress {
     from_port   = 8080
-    to_port = 8080
-    protocol = "tcp"
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port = 0
-    protocol = "-1"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -92,7 +93,7 @@ data "aws_ami" "latest_ubuntu_image" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"]  # Canonical account ID
+  owners = ["099720109477"] # Canonical account ID
 }
 output "aws_ami" {
   value = data.aws_ami.latest_ubuntu_image.id
@@ -103,19 +104,43 @@ output "ec2_public_ip" {
 }
 
 resource "aws_key_pair" "server-key" {
-  key_name = "server-key"
-  public_key = "${file(var.public_key_location)}"
+  key_name   = "server-key"
+  public_key = file(var.public_key_location)
 }
 
 resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.latest_ubuntu_image.id
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-  availability_zone = var.avail_zone
+  ami                         = data.aws_ami.latest_ubuntu_image.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  availability_zone           = var.avail_zone
   associate_public_ip_address = true
-  key_name = aws_key_pair.server-key.key_name
+  key_name                    = aws_key_pair.server-key.key_name
 
+  user_data = <<-EOF
+  #!/bin/sh 
+  sudo apt update -y 
+  sudo apt install docker.io -y
+  sudo apt install apache2 -y
+  sudo cp /tmp/index.html /var/www/html/index.html
+  sudo systemctl start docker
+  sudo usermod -a -G docker ubuntu
+  docker run -p 8080:80 nginx
+  EOF
+
+
+ provisioner "file" {
+   source = "index.html"
+   destination = "/tmp/index.html"
+   connection {
+     type        = "ssh"
+     user = "ubuntu"
+     private_key = file("${var.private_key_location}")
+     host = "${self.public_ip}"
+   }
+ }
+
+
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   tags = {
     Name = "${var.env_prefix}-server"
   }
